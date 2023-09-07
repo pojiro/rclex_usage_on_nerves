ba89d3d1dc4358864590dd9b80bcc9b80d14ac73
Ryota Kinukawa <kinukawa@tombo-works.com>
2023-09-07 10:52:47 +0900

    Initial commit

---
+++ lib/rclex_usage_on_nerves.ex
@@ -0,0 +1,18 @@
+defmodule RclexUsageOnNerves do
+  @moduledoc """
+  Documentation for RclexUsageOnNerves.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RclexUsageOnNerves.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rclex_usage_on_nerves_test.exs
@@ -0,0 +1,8 @@
+defmodule RclexUsageOnNervesTest do
+  use ExUnit.Case
+  doctest RclexUsageOnNerves
+
+  test "greets the world" do
+    assert RclexUsageOnNerves.hello() == :world
+  end
+end
